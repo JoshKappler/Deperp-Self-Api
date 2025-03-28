16904fa6b007a6d8c3714cf389afac9ea7fdb88d
JoshKappler <74198656+JoshKappler@users.noreply.github.com>
2025-03-28 15:54:45 -0500

    Update config.ts
    asdf

--- src/config.ts
+++ src/config.ts
@@ -9,13 +9,13 @@ type Config = {
 
 const testnetConfig: Config = {
     rpcUrl: 'https://arb1.arbitrum.io/rpc',
-    chainId: , 42161
+    chainId: 42161
     address: '0xDBaabB95757Fe4a5C9ffF0107201a74C90024488' 
 }
 
 const mainnetConfig: Config = {
     rpcUrl: 'https://arb1.arbitrum.io/rpc', //Rate limited and not for production systems.
-    chainId: 42161,
+    chainId: 42161
     address: '0xDBaabB95757Fe4a5C9ffF0107201a74C90024488' // Taurus GA
 }
 
